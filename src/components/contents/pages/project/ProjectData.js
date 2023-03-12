0dbc2c3c495ff81717e4263283d3f387a96e66ba
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-12 14:49:06 +0100

    Add page and contents of project page

---
+++ src/components/contents/pages/project/ProjectData.js
@@ -0,0 +1,89 @@
+/* eslint-disable import/prefer-default-export */
+export const projects = [
+  /* eslint-enable import/prefer-default-export */
+  {
+    /* eslint-disable global-require */
+    name: 'Math Magicians',
+    image: require('../../../../assets/math-m.png'),
+    description:
+      '"Math Magicians" is a website for all fans and lovers of mathematics. It is a Single Page App (SPA) that allows users make simple calculations and read random math-related quotes.',
+    technologies: ['Javascript', 'React', 'Css'],
+    liveLink: 'https://silver-queijadas-541756.netlify.app/',
+    githubLink: 'https://github.com/Stanleeeeee/Math-Magician',
+  },
+  {
+    name: 'Book Store',
+    image: require('../../../../assets/Bookstore.png'),
+    description: 'A web application for adding, removing and editing books. Users can see the chapter and percentage they have read in the dashboard.',
+    technologies: ['Javascript', 'React', 'CSS', 'Modules'],
+    liveLink:
+      'https://deploy-preview-7--incandescent-raindrop-b2641e.netlify.app',
+    githubLink: 'https://github.com/Stanleeeeee/Book-Store',
+  },
+  {
+    name: 'Weather App',
+    image: require('../../../../assets/weather-app.png'),
+    description:
+      'Global Weather App is a mobile application that has a categories of countries in the home page, users can access weather of every country. The React capstone project is about building a mobile web application to check a list of metrics (numeric values) that you will create making use of restful API',
+    technologies: ['Javascript', 'React', 'Redux', 'CSS', 'API'],
+    liveLink: 'https://deploy-preview-3--stan-weatherapp.netlify.app/',
+    githubLink:
+      'https://github.com/Stanleeeeee/GlobalWeather-ReactRedux-Capstone',
+  },
+  {
+    name: 'Leaderboard',
+    image: require('../../../../assets/LeaderBoard.png'),
+    description:
+      'The leaderboard website displays scores submitted by different players. After clicking submit, the player should click refresh in order to update the leaderboard.',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Modules', 'Webpack'],
+    liveLink: 'https://stanleeeeee.github.io/Leaderboard/dist/index.html',
+    githubLink: 'https://github.com/Stanleeeeee/Leaderboard',
+  },
+  {
+    name: 'Stanmega-techWeb Development Course',
+    image: require('../../../../assets/stanmega.png'),
+    description:
+      'The project is about Stanmega-tech Web Developmen Course. The website gives information about the course, what languages you can learn, when the classes start, and features some of the teachers.',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Modules', 'Webpack'],
+    liveLink: 'https://stanleeeeee.github.io/First-Capstone/',
+    githubLink: 'https://github.com/Stanleeeeee/First-Capstone',
+  },
+  {
+    name: 'Kino-Krypto App',
+    image: require('../../../../assets/kino-kypto.png'),
+    description:
+      'This is a Single Page App (SPA) react Capstone project that is built using API. The user(s) can select and see the current value and fluatuation of TOP 10 CRYPTO-CURRENCIES.',
+    technologies: ['React', 'Redux', 'CSS', 'Javascript'],
+    liveLink: 'https://deploy-preview-2--kino-crypto.netlify.app/',
+    githubLink:
+      'https://github.com/Stanleeeeee/Kino-Crypto-App-ReactReduxCapstone',
+  },
+  {
+    name: 'vet_clinic-Database',
+    image: require('../../../../assets/Database-Vet-Clinic.png'),
+    description:
+      'In this project, I used a relational database to create the data structure for a vet clinic. Some of what i did are Understand the different types of relationships between tables. Prepare complex queries that answer analytical questions. Use primary key & foreign key mechanism for joining tables. N.B: LiveLink will take you to the Schema.sql file',
+    technologies: ['PostgreSQL', 'Database', 'Diagram'],
+    liveLink:
+      'https://github.com/Stanleeeeee/vet_clinic-Database/blob/development/schema.sql',
+    githubLink: 'https://github.com/Stanleeeeee/vet_clinic-Database',
+  },
+  {
+    name: 'To-Do List',
+    image: require('../../../../assets/To-doList.png'),
+    description:
+      'A simple To-Do List App. The list will was styled according to the specifications listed in the Readme file. This simple web page was built using webpack and served by a webpack dev server.',
+    technologies: ['HTML', 'Javascript', 'CSS', 'Webpack', 'Modules'],
+    liveLink: 'https://stanleeeeee.github.io/To-Do-List/dist/index.html',
+    githubLink: 'https://github.com/Stanleeeeee/To-Do-List',
+  },
+  {
+    name: 'Awesome Book',
+    image: require('../../../../assets/awesomeBook.png'),
+    description:
+      ' A basic website that allows users to add/remove books from a list. I achieve that by using JavaScript objects and arrays, dynamically modify the DOM and add basic events and also Made sure that data is preserved in the browsers memory by using localStorage',
+    technologies: ['HTML', 'Javascript', 'CSS', 'luxon Library'],
+    liveLink: 'https://stanleeeeee.github.io/Awesome-book-solo-project/',
+    githubLink: 'https://github.com/Stanleeeeee/Awesome-book-solo-project',
+  },
+];
