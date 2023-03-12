7d4c209020948f325237ed0ea059e573ea16319e
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-12 14:45:21 +0100

    Add arrays of skills learned

---
+++ src/components/contents/pages/skills/skillsData.js
@@ -0,0 +1,86 @@
+export const sectionNames = ['Front-End', 'Back-End', 'Styles', 'Tools'];
+
+export const listItems = [
+  [
+    {
+      /* eslint-disable global-require */
+      icon: require('../../../../assets/icon/html.png'),
+      name: 'HTML',
+    },
+    {
+      icon: require('../../../../assets/icon/js.png'),
+      name: 'JavaScript',
+    },
+    {
+      icon: require('../../../../assets/icon/react.png'),
+      name: 'React',
+    },
+    {
+      icon: require('../../../../assets/icon/redux.png'),
+      name: 'Redux',
+    },
+    {
+      icon: require('../../../../assets/icon/mui.png'),
+      name: 'Material UI',
+    },
+  ],
+  [
+    {
+      icon: require('../../../../assets/icon/nodejs.png'),
+      name: 'NodeJS',
+    },
+    {
+      icon: require('../../../../assets/icon/mongodb.png'),
+      name: 'MongoDB',
+    },
+    {
+      icon: require('../../../../assets/icon/mysql.png'),
+      name: 'MySQL',
+    },
+  ],
+  [
+    {
+      icon: require('../../../../assets/icon/css.png'),
+      name: 'CSS',
+    },
+    {
+      icon: require('../../../../assets/icon/sass.png'),
+      name: 'Sass',
+    },
+    {
+      icon: require('../../../../assets/icon/bootstrap.png'),
+      name: 'Bootstrap',
+    },
+    {
+      icon: require('../../../../assets/icon/figma.png'),
+      name: 'Figma',
+    },
+  ],
+
+  [
+    {
+      icon: require('../../../../assets/icon/command-line.png'),
+      name: 'Command Line',
+    },
+    {
+      icon: require('../../../../assets/icon/npm.png'),
+      name: 'NPM',
+    },
+    {
+      icon: require('../../../../assets/icon/webpack.png'),
+      name: 'Webpack',
+    },
+    {
+      icon: require('../../../../assets/icon/git.png'),
+      name: 'Git',
+    },
+    {
+      icon: require('../../../../assets/icon/github.png'),
+      name: 'GitHub',
+    },
+    {
+      icon: require('../../../../assets/icon/jest.png'),
+      name: 'Jest',
+    },
+  ],
+];
