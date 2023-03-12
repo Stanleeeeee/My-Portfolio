e1228790dcc1f38e315412d0e9c06a30f558918e
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-12 14:56:05 +0100

    Add styles for main page

---
+++ src/styles/mainContainerStyles.js
@@ -0,0 +1,19 @@
+import background from '../assets/header-bg1.webp';
+
+export const styles = {
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  width: { lg: 'calc(100% - 200px)', md: '100%' },
+  marginLeft: 'auto',
+  '@media (max-width: 1200px)': {
+    height: 'auto',
+    minHeight: '100vh',
+    backgroundAttachment: 'fixed',
+  },
+};
