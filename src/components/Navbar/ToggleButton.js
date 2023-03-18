da0a5632a19773018b48be0500e11da51638ae39
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-03-18 13:02:04 +0100

    Fix toggle menu error with props

--- src/components/Navbar/ToggleButton.js
+++ src/components/Navbar/ToggleButton.js
@@ -1,9 +1,13 @@
 import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { styles } from '../../styles/sidebarStyles';
 
 function ToggleButton({ handleDrawerToggle }) {
+  ToggleButton.propTypes = {
+    handleDrawerToggle: PropTypes.isRequired,
+  };
   return (
     <Box sx={styles.toggleContainer}>
       <IconButton
